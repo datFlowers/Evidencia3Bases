e3b753fdf14de7e6b063d08688d822753bfc8497
datFlowers <168865635+datFlowers@users.noreply.github.com>
2024-05-04 00:48:14 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Driver;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.Core.Misc;
+using MongoDB;
+using P2.Colecciones;
+using System.Xml.Serialization;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        
+        private static MongoClient client;
+        private static IMongoDatabase storeDB;
+        private static IMongoCollection<Peliculas> MoviesCollection;
+        private static IMongoCollection<VentaBoletos> SalesCollection;
+
+        static Program()
+        {
+            client = new MongoClient("mongodb://localhost:27017/");
+            storeDB = client.GetDatabase("Cinepolis");
+            MoviesCollection = storeDB.GetCollection<Peliculas>("Peliculas");
+            SalesCollection = storeDB.GetCollection<VentaBoletos>("VentaBoletos");
+        }
+    
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
